8b525badabc1fe6eb8210d774b303a87cb7fa8d7
Luis Martinez <rickymtz1999@gmail.com>
2023-04-29 12:23:54 -0500

    add an endpoint for login

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const es6Renderer = require("express-es6-template-engine");
+const {checkAuth} = require('./middleware');
+
 const server = express();
+server.use(express.json());
+server.use(checkAuth)
 
 const corsOptions = {
     origin: 'http://127.0.0.1:5500'
@@ -20,6 +24,10 @@ server.get('/heartbeat', (req, res) => {
     res.json({
         "message": "Heartbeat"
     });
+});
+
+server.post('/auth/login', (req, res) => {
+    res.send(`${req.body.username}, ${req.body.password}`);
 });
 
 server.listen(8080, () => {
